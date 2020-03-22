641d3ad2d5ba4c161a042c486b2595e98aca059a
Roger Bi <roger.bi@flexigroup.com.au>
2020-03-23 09:31:48 +1100

    increase LAUNCH_TIME_CHECK_ENDS

--- Model/Ui/ConfigProvider.php
+++ Model/Ui/ConfigProvider.php
@@ -24,7 +24,7 @@ final class ConfigProvider implements ConfigProviderInterface
 
     const LAUNCH_TIME_URL = 'https://humm-variables.s3-ap-southeast-2.amazonaws.com/nz-launch-time.txt';
     const LAUNCH_TIME_DEFAULT = "2020-05-11 00:00:00 UTC";
-    const LAUNCH_TIME_CHECK_ENDS = "2020-05-18 00:00:00 UTC";
+    const LAUNCH_TIME_CHECK_ENDS = "2020-11-18 00:00:00 UTC";
 
     protected $_gatewayConfig;
     protected $_scopeConfigInterface;
