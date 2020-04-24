fa9768af88df6273e8a4fd1a5a575dd5bf000f67
Roger Bi <roger.bi@flexigroup.com.au>
2020-04-24 14:41:37 +1000

    rebuild success section

--- Controller/Checkout/Success.php
+++ Controller/Checkout/Success.php
@@ -6,7 +6,6 @@ use Magento\Sales\Model\Order;
 use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
-use Magento\Setup\Exception;
 
 /**
  * roger.bi@flexigroup.com.au
@@ -20,64 +19,58 @@ class Success extends AbstractAction implements CsrfAwareActionInterface
      */
     public function execute()
     {
-        $isAsyncCallback = $_SERVER['REQUEST_METHOD'] === "POST" ? true : false;
         $params = $this->getRequest()->getParams();
         $isValid = $this->getCryptoHelper()->isValidSignature($this->getRequest()->getParams(), $this->_encrypted->processValue($this->getGatewayConfig()->getApiKey()));
         $result = $params['x_result'];
         list($orderId, $hummProtectCode) = explode("-", $params['x_reference']);
         $transactionId = $params['x_gateway_reference'];
         $merchantNo = $params['x_account_id'];
-        $orderDue = $params['x_amount'];
         $order = $this->getOrderById($orderId);
+        $mesg = array();
+        $errorMsg = array();
+        $redirectErrorURL = '';
         $merchantNumber = $this->getGatewayConfig()->getMerchantNumber();
-
-        $mesg = sprintf("CallBack Start: Order ProtectCode [Web:%s] [Humm:%s] | MerchantNo [web:%s] [Humm:%s]|[Response---%s] [method--%s]", $order->getProtectCode(), $hummProtectCode, $merchantNumber, $merchantNo, json_encode($this->getRequest()->getParams()), $this->getRequest()->getMethod());
-        $this->getHummLogger()->log($mesg);
-        $this->getHummLogger()->log('Client IP:' . $this->getClientIP(), True);
+        $mesg[] = sprintf("CallBack Start: Order ProtectCode [Web:%s] [Humm:%s] | MerchantNo [web:%s] [Humm:%s]|[Response---%s] [method--%s]", $order->getProtectCode(), $hummProtectCode, $merchantNumber, $merchantNo, json_encode($this->getRequest()->getParams()), $this->getRequest()->getMethod());
+        $mesg[] = sprintf("Client IP: %s", $this->getClientIP());
 
         if (($merchantNo != $this->getGatewayConfig()->getMerchantNumber()) || ($hummProtectCode != $order->getProtectCode())) {
-            $mesg = sprintf("ERROR: Order ProtectCode [Web:%s] [Humm:%s] | %s MerchantNo %s |[Response---%s] [method--%s]", $order->getProtectCode(), $hummProtectCode, $merchantNumber, $merchantNo, json_encode($this->getRequest()->getParams()), $this->getRequest()->getMethod());
-            $this->getHummLogger()->log($mesg);
-            $this->_redirect('humm/checkout/error');
-            return;
-        }
-
-
-        if ($this->getHummLogger()) {
-            $this->getHummLogger()->log(sprintf("[Response---:%s] [method = %s]", json_encode($this->getRequest()->getParams()), $this->getRequest()->getMethod()));
+            $errorMsg[] = sprintf("ERROR: Order ProtectCode [Web:%s] [Humm:%s] | %s MerchantNo %s |[Response---%s] [method--%s]", $order->getProtectCode(), $hummProtectCode, $merchantNumber, $merchantNo, json_encode($this->getRequest()->getParams()), $this->getRequest()->getMethod());
+            $redirectErrorURL = "humm/checkout/error";
         }
 
         if (!$isValid) {
-            if ($this->getHummLogger()) {
-                $this->getHummLogger()->log('Possible site forgery detected: invalid response signature.' . $transactionId);
-            }
-            $this->_redirect('humm/checkout/error');
-            return;
+            $errorMsg[] = sprintf("Possible site forgery detected: invalid response signature transactionId:[%s]", $transactionId);
+            if (!empty($redirectErrorURL))
+                $redirectErrorURL = "humm/checkout/error";
         }
 
         if (!$orderId) {
-            if ($this->getHummLogger()) {
-                $this->getHummLogger()->log("Humm returned a null order id. This may indicate an issue with the humm payment gateway.");
-            }
-            $this->_redirect('humm/checkout/error');
-
-            return;
+            $errorMsg[] = sprintf("Humm returned a null order id. This may indicate an issue with the humm payment gateway.");
+            if (!empty($redirectErrorURL))
+                $redirectErrorURL = "humm/checkout/error";
         }
 
 
         if (!$order) {
-            if ($this->getHummLogger()) {
-                $this->getHummLogger()->log("Humm returned an id for an order that could not be retrieved: $orderId");
-            }
-            $this->_redirect('humm/checkout/error');
+            $errorMsg[] = sprintf("\"Humm returned an id for an order that could not be retrieved", $orderId);
+            if (!empty($redirectErrorURL))
+                $redirectErrorURL = "humm/checkout/error";
+           }
 
+        if ($redirectErrorURL) {
+            foreach ( $errorMsg as $eachError){
+                $this->getHummLogger()->log($eachError,true);
+            }
+            $this->_redirect($redirectErrorURL);
             return;
-        }
+         }
+
+         array_walk($mesg, function($eachMesg){
+            $this->getHummLogger()->log($eachMesg,true);
+         });
 
         if ($result == "completed" && $order->getState() === Order::STATE_PROCESSING) {
-
             $this->_redirect('checkout/onepage/success', array('_secure' => false));
-
             return;
         }
 
@@ -117,29 +110,31 @@ class Success extends AbstractAction implements CsrfAwareActionInterface
                 $invoiceAutomatically = $this->getGatewayConfig()->isAutomaticInvoice();
                 if ($invoiceAutomatically) {
                     $this->invoiceOrder($order, $transactionId);
-                    if ($this->getHummLogger()) {
-                        $this->getHummLogger()->log("Humm invoice produced:" . $orderId);
-                    }
+                    $this->getHummLogger()->log("Humm invoice produced:" . $orderId);
                 }
-                if ($this->getHummLogger()) {
-                    $this->getHummLogger()->log("END Payment:Humm payment successful for orderID:" . $orderId);
-                }
+                $this->getHummLogger()->log("END Payment:Humm payment successful for orderID:" . $orderId);
                 $this->getMessageManager()->addSuccessMessage(__("Your payment with humm is complete"));
             } catch (\Exception $e) {
-                if ($this->getHummLogger()) {
-                    $this->getHummLogger()->log("Successful Update State/Status Error:" . $e->getMessage());
-                }
+                $this->getHummLogger()->log("Successful Update State/Status Error:" . $e->getMessage());
             }
             $this->_redirect('checkout/onepage/success', array('_secure' => false));
         } else {
             $this->_eventManager->dispatch('humm_payment_cancel', ['order' => $order, 'type' => $result]);
-            if ($this->getHummLogger()) {
-                $this->getHummLogger()->log('humm_payment_cancel' . $orderId);
-            }
+            $this->getHummLogger()->log('humm_payment_cancel' . $orderId);
             $this->getMessageManager()->addWarningMessage(__("humm payment is unsuccessful. Please Check"));
             $this->getMessageManager()->addErrorMessage(__("There was an error in the humm payment"));
             $this->_redirect('checkout/cart', array('_secure' => false));
         }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClientIP()
+    {
+        $objctManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $remote = $objctManager->get('Magento\Framework\HTTP\PhpEnvironment\RemoteAddress');
+        return $remote->getRemoteAddress();
     }
 
     /**
@@ -221,15 +216,5 @@ class Success extends AbstractAction implements CsrfAwareActionInterface
     public function validateForCsrf(RequestInterface $request): ?bool
     {
         return true;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getClientIP()
-    {
-        $objctManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $remote = $objctManager->get('Magento\Framework\HTTP\PhpEnvironment\RemoteAddress');
-        return $remote->getRemoteAddress();
     }
 }
